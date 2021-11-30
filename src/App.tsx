08b767c8bb0b079f35c33041c161151d638c0085
Joaovictor144 <jvmachado144@gmail.com>
2021-11-30 16:58:20 -0300

    pre process

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 import { GlobalStyle } from "./components/style/global";
 
 export function App() {
   return (
     <>
       <Header/>
+      <Main/>
       <GlobalStyle/>
     </>
   );
